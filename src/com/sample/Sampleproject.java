a02521c608f39fd25fac306333e3333a06251e5f
eswari6 <eshuyan070@gmail.com>
2019-10-31 13:48:56 -0400

    www.capitalone.com

---
+++ src/com/sample/Sampleproject.java
@@ -0,0 +1,7 @@
+package com.sample;
+
+public class Sampleproject {
+public static void main(String[] args) {
+	System.out.println("www.capitalone.com");
+}
+}
---
+++ src/com/sample/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author eswari
+ *
+ */
+package com.sample;
